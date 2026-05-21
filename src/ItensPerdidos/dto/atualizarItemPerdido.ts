8b5cc62e41454232c34c591c3ebab6ab8f5dc58b
Nathalia Martins <nathaliafloresmartins4550@gmail.com>
2026-05-20 23:25:46 -0300

    feat:items dto

--- src/ItensPerdidos/dto/atualizarItemPerdido.ts
+++ src/ItensPerdidos/dto/atualizarItemPerdido.ts
@@ -1,42 +1,29 @@
-import { IsEnum, IsNotEmpty, IsDateString } from 'class-validator';
-
-export enum localizacaoItem {
-    espaco_interno = 'espaco_interno',
-    espaco_externo = 'espaco_externo',
-    biblioteca = 'biblioteca',
-    andar_b = 'andar_b',
-    sala_200 = 'sala_200',
-    sala_202 = 'sala_202',
-    sala_203 = 'sala_203',
-    sala_300 = 'sala_300',
-    sala_302 = 'sala_302',
-    sala_303 = 'sala_303',
-}
-
-export enum statusItem {
-    encontrado = 'encontrado',
-    devolvido = 'devolvido',
-    descartado = 'descartado',
-}
+import { IsDateString, IsEnum, IsNotEmpty, IsOptional } from "class-validator";
+import { localizacaoItem, statusItem } from "./criarItemPerdido.dto";
 
 export class CriarItemDTO {
+    @IsOptional()
     @IsEnum(localizacaoItem, { message: 'A localização informada é inválida' })
     localizado_em!: localizacaoItem;
 
+    @IsOptional()
     @IsEnum(statusItem, { message: 'O status informado é inválido' })
     status!: statusItem;
 
+    @IsOptional()
     @IsNotEmpty({ message: 'O nome não pode ser vazio' })
     nome!: string;
 
+    @IsOptional()
     @IsNotEmpty({ message: 'A descrição não pode ser vazia' })
     descricao!: string;
 
+    @IsOptional()
     //  formato de data (ex: "2026-05-20")
     @IsDateString({}, { message: 'A data deve estar em um formato ISO válido (AAAA-MM-DD)' })
     data_recebido!: string;
 
+    @IsOptional()
     @IsNotEmpty({ message: 'Imagem não pode ser vazio' })
     imagem!: string;
 }
-
