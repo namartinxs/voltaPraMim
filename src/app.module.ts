fafdc59649ec9b5116ae805da2c2e7f9ddc898a3
Nathalia Martins <nathaliafloresmartins4550@gmail.com>
2026-06-08 23:20:03 -0300

    feat: add AuthModule and ChamadoModule to AppModule imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,16 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { PostgresConfigService } from './config/postgres.config.service';
 import { ConfigModule } from '@nestjs/config';
 import { ItensPerdidosModule } from './ItensPerdidos/itensPerdidos.module';
+import { AuthModule } from './auth/auth.module';
+import { ChamadoModule } from './chamados/chamado.module';
+;
 
 @Module({
   imports: [
+    AuthModule,
     UsuarioModule,
     ItensPerdidosModule,
+    ChamadoModule,
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRootAsync({
       useClass: PostgresConfigService,
