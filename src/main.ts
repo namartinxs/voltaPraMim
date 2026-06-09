2103f0abfe4f4e791259c1871414baf670baf565
Nathalia Martins <nathaliafloresmartins4550@gmail.com>
2026-06-08 23:20:28 -0300

    feat: enhance main.ts with improved app initialization and middleware setup

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,40 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { useContainer } from 'class-validator';
 
+import { AppModule } from './app.module';
+import cookieParser from 'cookie-parser';
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app =
+    await NestFactory.create<NestExpressApplication>(AppModule);
+
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
       whitelist: true,
-      forbidNonWhitelisted: true
-    })
-  )
-  useContainer(app.select(AppModule),{fallbackOnErrors:true});
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+
+
+  app.use(cookieParser());
+
+  app.enableCors({
+    origin: process.env.FRONT_URL,
+    credentials: true,
+  });
+
+  app.useStaticAssets('uploads', {
+    prefix: '/uploads/',
+  });
+
+  useContainer(app.select(AppModule), {
+    fallbackOnErrors: true,
+  });
+
   await app.listen(3000);
 }
+
 bootstrap();
